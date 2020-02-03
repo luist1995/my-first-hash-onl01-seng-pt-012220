62e414dc8742021211229916f1a3179894a7ee5c
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-03 02:18:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+hash = {"Luis"=>"Future SE"}
   
 end
 
@@ -42,5 +42,5 @@ def adding
 
 end
 
-hash = {"Luis"=>"Future SE"}
+
 
